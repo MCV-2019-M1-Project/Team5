d1e18d6320522c1eab3ad4d39b048bf47e83d659
Joan Fontanals <jfontanals@ntent.com>
2019-10-03 19:06:28 +0200

    Start working with masks

--- ImageDescriptors/Histogram.py
+++ ImageDescriptors/Histogram.py
@@ -14,13 +14,25 @@ class Histogram:
             """If required, convert to HSV space"""
             self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
         """TODO: For QS2 pictures compute mask here"""
-        self.mask = None
         self.normalize = normalize
 
-    def histogram(self):
+    def histogram(self, mask=None):
         """Compute the histogram of the immage with the defined sizes and channels. Pass a mask which might be None"""
-        histogram = cv2.calcHist([self.img], CHANNELS, self.mask, HIST_SIZE, HIST_RANGE)
+        histogram = cv2.calcHist([self.img], CHANNELS, mask, HIST_SIZE, HIST_RANGE)
         if self.normalize:
             """If required normalize the histogram"""
             cv2.normalize(histogram, histogram, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
         return histogram
+
+
+class MaskedHistogram(Histogram):
+
+    def __init__(self, path, hsv=False, normalize=False):
+        super.__init__(path, hsv, normalize)
+        unmasked_histogram = self.histogram()
+        
+        self.maskedImg = None
+
+    def maskedHistogram(self):
+        self.histogram(self.mask)
+
